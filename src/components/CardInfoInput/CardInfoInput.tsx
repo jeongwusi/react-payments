d7e1b5985d7dd4332be1490103838c9c14f949eb
jeongwusi <jeongwusi@gmail.com>
2023-04-23 14:37:56 +0900

    ♻️ refactor: PropsWithChildren 타입을 이용

--- src/components/CardInfoInput/CardInfoInput.tsx
+++ src/components/CardInfoInput/CardInfoInput.tsx
@@ -1,13 +1,12 @@
-import { ReactNode } from 'react';
+import { PropsWithChildren } from 'react';
 import styles from './CardInfoInput.module.css';
 
 type CardInfoInputProps = {
   title: string;
   numberOfLetter?: number[];
-  children: ReactNode;
 };
 
-const CardInfoInput = ({ title, numberOfLetter, children }: CardInfoInputProps) => {
+const CardInfoInput = ({ title, numberOfLetter, children }: PropsWithChildren<CardInfoInputProps>) => {
   return (
     <div>
       <div className={styles.info}>
